ff098b670d863e6f7cee491265a5499eef16762a
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-02-05 23:41:55 +0100

    Add Redux, Redux devtools, initial reducer and state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import contacts from './reducers/contacts';
+import App from './containers/App';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  contacts
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/contacts.js
@@ -0,0 +1,20 @@
+const initialState = [
+  {
+    name: 'Theodore Roosevelt',
+    starred: true
+  },
+  {
+    name: 'Abraham Lincoln',
+    starred: false
+  },
+  {
+    name: 'George Washington',
+    starred: false
+  }
+];
+
+const contacts = (state = initialState, action) => {
+  return state;
+};
+
+export default contacts;
